448974438b69bd8d104699fbbc3c1e63e547d7f7
Paolo Bejarano <paolo@kurios.la>
2024-08-08 18:13:56 -0500

    Set up railway and prod mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,17 +13,18 @@ import { IndividualOwnerModule } from './individualowner/individualowner.module'
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 3306),
-        username: configService.get<string>('DB_USERNAME', 'root'),
-        password: configService.get<string>('DB_PASSWORD', '12345678'),
-        database: configService.get<string>('DB_DATABASE', 'trebol'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
         autoLoadEntities: true,
         synchronize: true,
       }),
